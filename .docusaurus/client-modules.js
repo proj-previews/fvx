3909dff861eb30a8b10a514fbed8ddee608b64c9
hong li <lihongitchina@gmail.com>
2021-09-02 11:11:58 +0800

    clean

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/lihong/workbench/dev/src/github.com/proj-previews/fvx/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/lihong/workbench/dev/src/github.com/proj-previews/fvx/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/lihong/workbench/dev/src/github.com/proj-previews/fvx/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/lihong/workbench/dev/src/github.com/proj-previews/fvx/src/css/custom.css"),
-];
